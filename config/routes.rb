e568c53406ac1d23cc9e7c93d0fb5c87e7ec7cda
carlton <carltonmbaja0@gmail.com>
2022-10-13 21:11:35 +0300

    Added serializer

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :comments, only:[:index, :show, :update, :create]
   resources :blogs, only:[:index, :show, :update, :create]
 
+  # get "/blogcomments" to: "blogs#show"
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
   delete "/logout", to: "sessions#destroy"
