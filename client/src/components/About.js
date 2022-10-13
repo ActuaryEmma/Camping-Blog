17f1380c4c83ae2b9f25e5375580e75af7d69abc
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-10-14 00:28:11 +0300

    add about styles

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,7 +9,7 @@ function About() {
             <div className="head">
                 <h1>About CampKe</h1>
             </div>
-            <div className="body">
+            <div className="infor">
                 
                     <p>
                     CampKe is a PLA camping enthusiasts who, for over 10 years, have joined together to share  knowledge and experience and to ensure the quality of camp programs in line with the Tembea Kenya Initiative. Because of the diverse membership and exceptional programs, children and adults have the opportunity to learn powerful lessons in community, character-building, skill development, and healthy living - lessons that can be learned nowhere else. Find the latest blog postings for camp and youth development professionals, including updates on our 5-year Magical Kenya Campaign.
